a39c03fd4583c8cbf71bb14e749101c48bc03e06
jamiegilmartin <jamiegilmartin@gmail.com>
2013-09-16 13:05:54 -0400

    add series, clean up bash

--- timelapse.sh
+++ timelapse.sh
@@ -5,10 +5,11 @@ SAVEDIR = /home/pi/sketchbook/timeLapse/images
 
 while [ true ]; do
 
-filename=$ROLL=$(date -u + "%d%m%Y_%H%M-%S").jpg
+filename=$ROLL-$(date -u + "%d%m%Y_%H%M-%S").jpg
 
 /opt/vc/bin/raspistill -o $SAVEDIR/$filename
 
 sleep 4;
 
 done;
+
